0a1f09210116bd34011c9ae6cf6844f2d2eadf92
kriss <462679766@qq.com>
2025-01-08 14:24:50 +0800

    feature: monorepo

---
+++ src/helper.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once __DIR__ . '/LaravelTranslation/helper.php';
+require_once __DIR__ . '/LaravelValidation/helper.php';
---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+if (base_path('/config/app.php')) {
+    copy_dir(__DIR__. '/config', base_path('/config'));
+}
+
+if (!is_dir(base_path('resource/translations'))) {
+    copy_dir(__DIR__. '/resource', base_path('/resource'));
+}
+
+require_once __DIR__ . '/../vendor/workerman/webman-framework/src/support/bootstrap.php';
